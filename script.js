7d0afbd56446a05c284df884e914d378f3a73d0d
NYUCT Design Labs <132071678+nyuct@users.noreply.github.com>
2026-02-11 17:17:07 +0530

    safari debug

--- script.js
+++ script.js
@@ -954,33 +954,33 @@ if (!isSafari) {
   });
 }
 
-// document.addEventListener("contextmenu", function (event) {
-//   event.preventDefault();
-// });
-// console.log(
-//   Object.defineProperties(new Error(), {
-//     toString: {
-//       value() {
-//         new Error().stack.includes("toString@") && alert("Safari devtools");
-//       },
-//     },
-//     message: {
-//       get() {
-//         alert("Close the Devtool");
-//       },
-//     },
-//   })
-// );
-// document.onkeydown = function (e) {
-//   switch (e.keyCode) {
-//     case 123: // F12
-//     case "I".charCodeAt(0): // Ctrl + Shift + I
-//     case "J".charCodeAt(0): // Ctrl + Shift + J
-//     case "U".charCodeAt(0): // Ctrl + U
-//     case "C".charCodeAt(0): // Ctrl + C
-//       return false;
-//   }
-// };
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+});
+console.log(
+  Object.defineProperties(new Error(), {
+    toString: {
+      value() {
+        new Error().stack.includes("toString@") && alert("Safari devtools");
+      },
+    },
+    message: {
+      get() {
+        alert("Close the Devtool");
+      },
+    },
+  })
+);
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+    case 123: // F12
+    case "I".charCodeAt(0): // Ctrl + Shift + I
+    case "J".charCodeAt(0): // Ctrl + Shift + J
+    case "U".charCodeAt(0): // Ctrl + U
+    case "C".charCodeAt(0): // Ctrl + C
+      return false;
+  }
+};
 
 document.querySelector(".hamburger").addEventListener("click", function (e) {
   e.preventDefault();
